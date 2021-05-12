5e3c1c7b28be1d5330a9ee07ff7861726f310283
Vladyslav2020 <vmskl2020@gmail.com>
2021-05-12 19:39:58 +0300

    Added reading from file to LectureDatabase class

--- KPI/Lecture.cs
+++ KPI/Lecture.cs
@@ -1,19 +1,23 @@
-﻿namespace KPI
+﻿using System.Collections.Generic;
+
+namespace KPI
 {
     public class Lecture
     {
         private static int ID = 0;
-        public int id;
-        public string name;
-        public string textOfLecture;
+        private int id;
+        public int number;
+        public string title;
+        public List<string> lectureBlocks;
         public Test test;
         
-        public Lecture(string name, string textOfLecture, Test test)
+        public Lecture(int nubmer, string title, List<string> lectureBlocks, Test test)
         {
             Lecture.ID++;
             this.id = Lecture.ID;
-            this.name = name;
-            this.textOfLecture = textOfLecture;
+            this.number = number;
+            this.title = title;
+            this.lectureBlocks = lectureBlocks;
             this.test = test;
         }
     }
--- KPI/LectureDataBase.cs
+++ KPI/LectureDataBase.cs
@@ -1,9 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
 
 namespace KPI
 {
     public class LectureDataBase
     {
         public List<Lecture> lectures;
+        public LectureDataBase()
+        {
+            this.lectures = this.getLectures();
+        }
+        public List<Lecture> getLectures()
+        {
+            List<Lecture> lectures = new List<Lecture>();
+            string[] files = Directory.GetFiles("./lectures");
+            for (int i = 0; i < files.Length; i++)
+            {
+                string name;
+                List<string> blocksOfLecture = new List<string>();
+                using (StreamReader sr = new StreamReader(files[i]))
+                {
+                    name = sr.ReadLine();
+                    string blockOfLecture = "";
+                    while (!sr.EndOfStream)
+                    {
+                        string str = sr.ReadLine();
+                        if (str.Count(ch => ch == '-') >= 50)
+                        {
+                            if (blockOfLecture.Length > 0)
+                                blocksOfLecture.Add(blockOfLecture);
+                            blockOfLecture = "";
+                        }
+                        else
+                            blockOfLecture = String.Concat(blockOfLecture, str, '\n');
+                    }
+
+                    if (blockOfLecture.Length > 0)
+                    {
+                        blocksOfLecture.Add((blockOfLecture));
+                        blockOfLecture = "";
+                    }
+                }
+                Lecture lecture = new Lecture(i + 1, name, blocksOfLecture, null);
+                lectures.Add(lecture);
+            }
+
+            return lectures;
+        }
     }
 }
