dfdfb32d536ea9aa71fe254560314c7e7aadc4f0
Sviatozar <svpetrenko123@gmail.com>
2021-05-12 16:18:41 +0300

    Add test classes

---
+++ KPI/TestQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPI
+{
+    class TestQuestion
+    {
+        public string question;
+        public string[] options;
+        public int answerIndex;
+
+        public TestQuestion(string question, string[] options, int answerIndex)
+        {
+            this.question = question;
+            this.options = options;
+            this.answerIndex = answerIndex;
+        }
+    }
+}
