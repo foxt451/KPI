9a4626a92651afd974699b83612bbfe597518d9d
Max <rendon4ik@gmail.com>
2021-05-26 21:10:28 +0300

    Adding Admin Class

---
+++ KPI/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace KPI
+{
+    public sealed class Admin : User
+    {
+        public Admin(Output output) : base(output) { }
+
+        public void AddLecture(string title, List<string> data)
+        {
+            LectureDataBase dataBase = new LectureDataBase();
+            dataBase.AddNewLecture(title, data);
+        }
+    }
+}
